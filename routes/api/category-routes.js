49f1483d36b2f88293b50b70d61418299cd4ce32
James <James.Hacklander@gmail.com>
2021-06-09 11:14:13 -0700

    Included Product model for get all in category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,7 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll().then((category) => {
+  Category.findAll({include: [{model: Product}]}).then((category) => {
     res.status(200).json(category)
   }).catch((err) => {
     res.status(400).json(err)
